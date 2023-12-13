7092ab7f2a78f6143e8493f394961333fa9a6543
murilo-marian <murilomarian.mm@gmail.com>
2023-12-12 21:22:31 -0300

    add eclipse support

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,17 +2,14 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         Filtro filtro = Filtro.getInstance("blackList.txt", TipoDeFiltro.Asteriscos, true);
-        /*filtro.adicionarBlackList("filho da puta");*/
-
-        //TODO ARTIGO
-
-        filtro.atualizarBlacklist();
-        filtro.hasCaminhoLog("log.json");
-
-        System.out.println(filtro.filtrar("", "puta do caralho", Calendar.getInstance().getTime()));
+        Scanner entrada = new Scanner(System.in);
+        while(true) {
+            filtro.adicionarBlackList(entrada.nextLine());
+        }
     }
 }
